95c5301a5c0eb78e09168a2db1cd5dd2becc098a
ManueleSeretti <136794705+ManueleSeretti@users.noreply.github.com>
2023-11-10 09:31:14 +0100

    Initial commit

---
+++ src/main/java/ManueleSeretti/progettou5w2/ProgettoU5W2Application.java
@@ -0,0 +1,13 @@
+package ManueleSeretti.progettou5w2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU5W2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU5W2Application.class, args);
+	}
+
+}
